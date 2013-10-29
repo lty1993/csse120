252d34805336297728b43310e45eaf9f5ee5c369
Tianyu Liu <lty1993@gmail.com>
2013-10-29 14:45:43 -0400

    new class

---
+++ src/our_create.py
@@ -0,0 +1,9 @@
+"""
+This module is a slightly revised implemenation of the   new_create   module
+originally designed by Zach Dodds at Harvey Mudd and David Mutchler at Rose-Hulman.
+
+Revisions by Tianyu Liu, Xiangqing Zhang and Matthew O'Brien, October 2013.
+""" 
+import new_create
+
+class Our_create(new_create.Create):
