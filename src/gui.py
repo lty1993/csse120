26f2fc95058b5dceb24e153cdd71363aee33bfb6
obrienm4 <obrienm@rose-hulman.edu>
2013-10-29 18:31:54 -0400

    gui for autonomous control of robot

--- src/gui.py
+++ src/gui.py
@@ -1,25 +1,50 @@
-# import Robot.py
+import Robot
 import tkinter
 from tkinter import ttk
 
-class gui(object):
+class Gui():
   def __init__(self):
+      self.speed = None
+      self.rotation = None
+      self.time = None
+
+def main():
+    g = Gui()
+    """
+    A gui used to autonomously control the robot with user-specified speed, rotation, and time.
+    Contributor: Matthew O'Brien
+    """
     root = tkinter.Tk()
     frame = ttk.Frame(root, padding=(20, 30), relief='raised')
     frame.grid()
-    move_forward_button = ttk.Button(frame, text='Forward')
-    move_forward_button.grid(row=0, column=0)
-    move_forward_button['command'] = lambda : move_forward()
-    move_backward_button = ttk.Button(frame, text='Backward')
-    move_backward_button.grid(row=1, column=0)
-    move_backward_button['command'] = lambda : move_backward()
-    curve_right_button = ttk.Button(frame, text='Curve Right')
-    curve_right_button.grid(row=0, column=1)
-    curve_right_button['command'] = lambda : curve_right()
-    curve_left_button = ttk.Button(frame, text='Curve Left')
-    curve_left_button.grid(row=1, column=1)
-    curve_left_button['command'] = lambda : curve_left()
+
+    speed_entry_label = ttk.Label(frame, text='Enter a speed')
+    speed_entry_label.grid(row=0, column=0)
+    speed_entry = ttk.Entry(frame, width=4)
+    speed_entry.grid(row=0, column=1)
+    g.speed = tkinter.IntVar()
+    speed_entry['textvariable'] = g.speed
+
+    rotation_entry_label = ttk.Label(frame, text='Enter a rotational speed')
+    rotation_entry_label.grid(row=1, column=0)
+    rotation_entry = ttk.Entry(frame, width=4)
+    rotation_entry.grid(row=1, column=1)
+    g.rotation = tkinter.IntVar()
+    rotation_entry['textvariable'] = g.rotation
+
+    time_entry_label = ttk.Label(frame, text='Enter a time')
+    time_entry_label.grid(row=2, column=0)
+    time_entry = ttk.Entry(frame, width=4)
+    time_entry.grid(row=2, column=1)
+    g.time = tkinter.IntVar()
+    time_entry['textvariable'] = g.time
+
+    start_button = ttk.Button(frame, text='Start')
+    start_button.grid(row=3, column=0)
+    start_button['command'] = lambda : Robot.move_autonomously(g.speed.get(), g.rotation.get(), g.time.get())
+
     root.mainloop()
 
 
-g = gui()
+if __name__ == '__main__':
+    main()
