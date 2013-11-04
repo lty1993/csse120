7d6ba45dd92c0b876060a2049f547d8217593a18
zxqdx <zhangx2@rose-hulman.edu>
2013-11-04 16:13:42 -0500

    Task list updated

--- src/tasks-3.r
+++ src/tasks-3.r
@@ -1,10 +1,2 @@
-Matthew O'Brien
-
-DATE-STARTED, DATE-DUE/ENDED.  DESCRIBE-TASK.  STATUS.  HOURS.
-10-25           10-25. 				m3. 		DONE. 	0.5
-10-25 			10-26. 		     merge work	    DONE	1
-10-29 			10-29. 				f4. 		DONE.	1.5
-10-29			10-31				f2			DONE.	1
-10-30 			11-1.  				f7a-1       DONE.   2
-11-1			11-1.				f7a-2		DONE.   1
-10-31			11-1.			 	f1		    DONE.   1.5
+Name: Matthew O'Brien
+Total Working Hours for Sprint 1: 8 hrs
